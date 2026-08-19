1ff8de5dee6059b4cf002f3af86be2ed5fb2ba91
leelakrishnasainaidu <naiduleelakrishnasai@gmail.com>
2026-08-19 15:46:50 +0530

    Cart Screen UI Completed

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import { COLORS } from '@/constants'
+import { useCart } from '@/context/CartContext'
 
 export default function TabLayout() {
+
+   const { cartItems } = useCart();
+
    return (
       <Tabs
          screenOptions={{
@@ -25,7 +29,16 @@ export default function TabLayout() {
             tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "home" : "home-outline"} size={26} color={color} />
          }} />
          <Tabs.Screen name="cart" options={{
-            tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "cart" : "cart-outline"} size={26} color={color} />
+            tabBarIcon: ({ color, focused }) => (
+               <View className="relative">
+                  <Ionicons name={focused ? "cart" : "cart-outline"} size={26} color={color} />
+                  {cartItems.length > 0 && (
+                     <View className='absolute -top-2 -right-2 bg-accent size-3 rounded-full items-center justify-center'>
+                        <Ionicons name='ellipse' size={6} color="#fff" />
+                     </View>
+                  )}
+               </View>
+            )
          }} />
          <Tabs.Screen name="favorites" options={{
             tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "heart" : "heart-outline"} size={26} color={color} />
